cef466c220554032efa14bd4daf0b96e29e9b927
Sakethram17 <90086812+Sakethram17@users.noreply.github.com>
2022-02-06 19:07:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX=0;
+noseY=0;
+
+function peload(){
+console.log("preload");
+    clown_nose = loadImage("https://i.postimg.cc/MpF2yLzn/sticker-timeline-clown-nose-648014.png");
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    console.log("draw");
+    image(video, 0, 0, 300, 300);
+    console.log("draw1");
+   image(clown_nose, noseX, noseY, 30, 30);
+   console.log("draw2");
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
+    }
+}
+
